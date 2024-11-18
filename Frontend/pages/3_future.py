6e65c2bfb7890be6d32bce0ae124af0c7e84665b
lwilsen <lwilsen@umass.edu>
2024-11-18 18:31:47 -0500

    updated app

--- Frontend/pages/3_future.py
+++ Frontend/pages/3_future.py
@@ -8,8 +8,16 @@ Ideally it would be nice to have access to more advanced satellite imagery,
          but that's a little too expensive for the scope of this project.
 ''')
 
-st.header("More accurate ADS-B data")
+st.header("ADS-B data")
 st.write('''
 The ADS-B data I collected was only from a couple of days, and so when I pull more data, 
-         it will be easier to identify airports and helipads.
+         it will be easier to identify airports and helipads. 
+         I also am going to collect data on the actual types of the airplanes (model number) to better 
+         understand who might be using specific airports.
+''')
+
+st.header("Containerize")
+st.write('''
+Right now this whole app runs through streamlit, so 
+         I'm also going to create a fast-api backend and a docker container for this app.
 ''')
