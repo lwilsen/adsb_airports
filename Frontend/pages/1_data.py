dfd74f6b8b525a3e6c25468c908aa347578e2ac0
lwilsen <lwilsen@umass.edu>
2024-12-22 13:57:38 -0500

    streamlit toml

--- Frontend/pages/1_data.py
+++ Frontend/pages/1_data.py
@@ -16,38 +16,51 @@ import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
 
+st.title("Data Information")
 
-st.title("Data Exploration!")
+st.write('''### ADS-B Category Info''')
 
-st.write(
-    """I was able to identify when airplanes are on the ground using 
-         the alt_barom column. Then I used the latitude and longitude 
-         of those airplanes to make a prediction that there is an airport 
-         there."""
-)
+with st.expander("ADS-B aircraft category **A** information"):
+    st.subheader("A Category")
+    st.markdown(
+        """
+    * **A0:** No ADS-B emitter category information.
+    * **A1:** Light (< 15500 lbs) - Any airplane with a maximum takeoff weight less than 15,500 pounds.
+    * **A2:** Small (15500 to 75000 lbs) - Any airplane with a maximum takeoff weight greater than or equal to 15,500 pounds but less than 75,000 pounds.
+    * **A3:** Large (75000 to 300000 lbs) - Any airplane with a maximum takeoff weight greater than or equal to 75,000 pounds but less than 300,000 pounds that does not qualify for the high vortex category.
+    * **A4:** High vortex large (aircraft such as B-757) - Any airplane with a maximum takeoff weight greater than or equal to 75,000 pounds but less than 300,000 pounds that has been determined to generate a high wake vortex.
+    * **A5:** Heavy (> 300000 lbs) - Any airplane with a maximum takeoff weight equal to or above 300,000 pounds.
+    * **A6:** High performance (> 5g acceleration and 400 kts) - Any airplane, regardless of weight, which can maneuver in excess of 5 G's and maintain true airspeed above 400 knots.
+    * **A7:** Rotorcraft - Any rotorcraft regardless of weight.
+    """
+    )
+with st.expander("ADS-B aircraft category **B** information"):
+    st.subheader("B Category")
+    st.markdown(
+        """
+    * **B0:** No ADS-B emitter category information.
+    * **B1:** Glider / sailplane - Any glider or sailplane regardless of weight.
+    * **B2:** Lighter-than-air - Any lighter than air (airship or balloon) regardless of weight.
+    * **B3:** Parachutist / skydiver
+    * **B4:** Ultralight / hang-glider / paraglider - A vehicle that meets the requirements of 14 CFR § 103.1.
+    * **B5:** Reserved
+    * **B6:** Unmanned aerial vehicle - Any unmanned aerial vehicle or unmanned aircraft system regardless of weight.
+    * **B7:** Space / trans-atmospheric vehicle
+    """
+    )
 
-with open("grounded.pkl", "rb") as f:
-    grounded = pickle.load(f)
+with st.expander("ADS-B aircraft category **C** information"):
+    st.subheader("C Category")
+    st.markdown(
+        """
+    * **C0:** No ADS-B emitter category information.
+    * **C1:** Surface vehicle - emergency vehicle
+    * **C2:** Surface vehicle - service vehicle
+    * **C3:** Point obstacle (includes tethered balloons)
+    * **C4:** Cluster obstacle
+    * **C5:** Line obstacle
+    * **C6:** Reserved
+    * **C7:** Reserved
+    """
+    )
 
-st.table(grounded.head())
-
-fig1 = plt.figure(figsize=(11, 7))
-grounded["lat"].hist(bins=20)
-plt.xlabel("Latitude")
-plt.ylabel("Frequency")
-plt.title("Histogram of Latitude")
-st.pyplot(fig1)
-
-fig2 = plt.figure(figsize=(11, 7))
-grounded["lon"].hist(bins=20)
-plt.xlabel("Longitude")
-plt.ylabel("Frequency")
-plt.title("Histogram of Longitude")
-st.pyplot(fig2)
-
-fig4 = plt.figure(figsize=(11, 7))
-grounded["gs"].hist(bins=20)
-plt.xlabel("Ground Speed")
-plt.ylabel("Frequency")
-plt.title("Histogram of Ground Speed")
-st.pyplot(fig4)
--- Frontend/pages/2_airports.py
+++ Frontend/pages/2_airports.py
@@ -132,51 +132,6 @@ for hex_idx in hex_gjson_indx:
 filtered_df = h3_df[h3_df[f"H3_{RESOLUTION}_cell"].isin(h3cell_id_list)]
 st.write(filtered_df)
 
-"""ADS-B General info"""
-
-with st.expander("ADS-B aircraft category **A** information"):
-    st.subheader("A Category")
-    st.markdown(
-        """
-    * **A0:** No ADS-B emitter category information.
-    * **A1:** Light (< 15500 lbs) - Any airplane with a maximum takeoff weight less than 15,500 pounds.
-    * **A2:** Small (15500 to 75000 lbs) - Any airplane with a maximum takeoff weight greater than or equal to 15,500 pounds but less than 75,000 pounds.
-    * **A3:** Large (75000 to 300000 lbs) - Any airplane with a maximum takeoff weight greater than or equal to 75,000 pounds but less than 300,000 pounds that does not qualify for the high vortex category.
-    * **A4:** High vortex large (aircraft such as B-757) - Any airplane with a maximum takeoff weight greater than or equal to 75,000 pounds but less than 300,000 pounds that has been determined to generate a high wake vortex.
-    * **A5:** Heavy (> 300000 lbs) - Any airplane with a maximum takeoff weight equal to or above 300,000 pounds.
-    * **A6:** High performance (> 5g acceleration and 400 kts) - Any airplane, regardless of weight, which can maneuver in excess of 5 G's and maintain true airspeed above 400 knots.
-    * **A7:** Rotorcraft - Any rotorcraft regardless of weight.
-    """
-    )
-with st.expander("ADS-B aircraft category **B** information"):
-    st.subheader("B Category")
-    st.markdown(
-        """
-    * **B0:** No ADS-B emitter category information.
-    * **B1:** Glider / sailplane - Any glider or sailplane regardless of weight.
-    * **B2:** Lighter-than-air - Any lighter than air (airship or balloon) regardless of weight.
-    * **B3:** Parachutist / skydiver
-    * **B4:** Ultralight / hang-glider / paraglider - A vehicle that meets the requirements of 14 CFR § 103.1.
-    * **B5:** Reserved
-    * **B6:** Unmanned aerial vehicle - Any unmanned aerial vehicle or unmanned aircraft system regardless of weight.
-    * **B7:** Space / trans-atmospheric vehicle
-    """
-    )
-
-with st.expander("ADS-B aircraft category **C** information"):
-    st.subheader("C Category")
-    st.markdown(
-        """
-    * **C0:** No ADS-B emitter category information.
-    * **C1:** Surface vehicle - emergency vehicle
-    * **C2:** Surface vehicle - service vehicle
-    * **C3:** Point obstacle (includes tethered balloons)
-    * **C4:** Cluster obstacle
-    * **C5:** Line obstacle
-    * **C6:** Reserved
-    * **C7:** Reserved
-    """
-    )
 
 
 st.subheader("Choose satellite box width and height")
